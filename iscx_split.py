04d1dd544a34ee6aeff97b5981c6e3152b97b01b
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2016-10-20 12:31:29 +1300

    Program can load data and calculate k stratified folds.
    TODO: Write the k stratified folds into new files using a
    directory structure.

---
+++ iscx_split.py
@@ -0,0 +1,145 @@
+# Copyright 2016 Jarrod N. Bakker
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from lxml import etree
+from sklearn.cross_validation import StratifiedKFold
+import os
+
+__author__ = "Jarrod N. Bakker"
+__status__ = "Development"
+
+RAND_SEED = 99999999
+
+
+class ISCXSplit:
+    """This class takes care of splitting the ISCX 2012 DDoS dataset.
+
+    The resulting training and testing sets are k-fold stratified in
+    regards to the proportion of 'Normal' and 'Attack' cases. The
+    StratifiedKFold class from the sklearn package is used.
+
+    NOTE: The training set is traditionally larger than the testing
+    set in k-fold cross validation. This means that you train with k-1
+    folds and test with 1 fold. However this class switches the two
+    around so that the training set is small enough to allow a SVM to
+    train within a reasonable amount of time. This is only an issue as
+    the dataset contains 571698 cases. 534238 represent normal cases
+    and 37460 represent attack cases (checked 20/10/2016).
+    """
+
+    def __init__(self, folds, input_dir, files):
+        """Initialise.
+
+        :param folds: The desired number of folds.
+        :param input_dir: Directory of the XML dataset files.
+        :param files: List of files to read in.
+        """
+        self._folds = folds
+        self._input_dir = input_dir
+        self._files = files
+        self._raw_data = []
+        self._labels = []
+
+    def create_sets(self, output_dir):
+        """Read in the ISCX 2012 DDoS dataset and create stratified
+        k-fold training and testing sets.
+
+        The created training and testing sets are written to separate
+        directories.
+
+        :param output_dir: Directory for the created training and
+        testing sets to be written to.
+        """
+        self._load_data()
+        s_kfold = self._calc_kfold(self._labels)
+        # TODO Create the testing and training sets using the indices and write the raw xml data into new files.
+        pass
+
+    def _load_data(self):
+        """Read in the ISCX 2012 DDoS dataset and store the data and
+        the labels.
+        """
+        print("Loading ISCX 2012 DDoS dataset...")
+        for fname in self._files:
+            path = os.path.join(self._input_dir, fname)
+            raw_data, raw_labels = self._read_data(path)
+            self._raw_data.extend(raw_data)
+            self._labels.extend(raw_labels)
+
+    def _read_data(self, fname):
+        """Read data from an ISCX dataset XML.
+
+        :param fname: Name of the file to read the data from.
+        :return: The data and labels.
+        """
+        print("\tReading data from: {0}".format(fname))
+        data_etree = etree.parse(fname)
+        raw_data, raw_labels = self._etree_to_dict(data_etree)
+        print("\t\tLoading complete.")
+        return raw_data, raw_labels
+
+    def _etree_to_dict(self, etree):
+        """Convert an XML etree into a list of dicts.
+
+        This method only takes care of elements, not attributes!
+
+        :param etree: Etree object to process
+        :return: Data as a list of dict.
+        """
+        root = etree.getroot()
+        data = []
+        labels = []
+        for flow in root:
+            flow_data = {}
+            for i in range(len(flow)):
+                if flow[i].tag != "Tag":
+                    flow_data[flow[i].tag] = flow[i].text
+                else:
+                    if flow[i].text == "Normal":
+                        labels.append(TagValue.Normal)
+                    else:
+                        labels.append(TagValue.Attack)
+            data.append(flow_data)
+        return data, labels
+
+    def _calc_kfold(self, labels):
+        """Calculate k stratified folds.
+
+        :param labels: The labels for the dataset elements.
+        :return: StratifiedKFold object representing what dataset
+        elements belong in each fold.
+        """
+        print("Calculating {0} stratified folds...".format(self._folds))
+        s_kfold = StratifiedKFold(labels, n_folds=self._folds,
+                                  shuffle=True, random_state=RAND_SEED)
+        print("\tFold calculation complete.")
+        return s_kfold
+
+
+class TagValue:
+    """Enum for the dataset tag labels.
+    """
+    Normal = 0
+    Attack = 1
+
+if __name__ == "__main__":
+    folds = 5
+    input_dir = "/vol/nerg-solar/bakkerjarr/Datasets/ISCXIDS2012" \
+                    "/labeled_flows_xml/"
+    files = ["TestbedTueJun15-1Flows.xml",
+             "TestbedTueJun15-2Flows.xml",
+             "TestbedTueJun15-3Flows.xml"]
+    output_dir = "ISCX2012DDoS_5-fold_sets"
+    split = ISCXSplit(folds, input_dir, files)
+    split.create_sets(output_dir)
